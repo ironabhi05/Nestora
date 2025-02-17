c4850232a13422d9deb03ab94bc265a3cc1dd9d9
iron abhi <ironabhi728@gmail.com>
2025-02-18 01:13:19 +0530

    Update listings.js

--- routes/listings.js
+++ routes/listings.js
@@ -1,27 +1,34 @@
 const express = require("express");
 const router = express.Router();
 const Listing = require("../models/listing.js");
-const wrapAsync = require('../utils/wrapAsync.js');
-const { isLoggedIn, listingValidate, isOwner } = require('../Middleware.js');
-const listingController = require('../controllers/listing.js');
+const wrapAsync = require("../utils/wrapAsync.js");
+const { isLoggedIn, listingValidate, isOwner } = require("../Middleware.js");
+const listingController = require("../controllers/listing.js");
 
 //Index Listing
-router.route("/")
-    .get(wrapAsync(listingController.indexRoute))
-    .post(isLoggedIn, wrapAsync(listingController.createPostNewRoute));
+router
+  .route("/")
+  .get(wrapAsync(listingController.indexRoute))
+  .post(isLoggedIn, wrapAsync(listingController.createPostNewRoute));
 
 //Create Listing
 //GET
 router.get("/new", isLoggedIn, listingController.createGetNewRoute);
 
 //Show Listing
-router.route("/:id")
-    .get(wrapAsync(listingController.showListingRoute))
-    .put(isLoggedIn, isOwner, wrapAsync(listingController.editPutRoute))
-    .delete(isLoggedIn, isOwner, wrapAsync(listingController.deleteListingRoute));
+router
+  .route("/:id")
+  .get(wrapAsync(listingController.showListingRoute))
+  .put(isLoggedIn, isOwner, wrapAsync(listingController.editPutRoute))
+  .delete(isLoggedIn, isOwner, wrapAsync(listingController.deleteListingRoute));
 
 //Edit Listing
 //GET
-router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingController.editGetRoute));
+router.get(
+  "/:id/edit",
+  isLoggedIn,
+  isOwner,
+  wrapAsync(listingController.editGetRoute)
+);
 
 module.exports = router;
